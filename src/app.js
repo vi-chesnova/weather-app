dc885127a0d3c787a1a3e3e606149d14ce163355
vi-chesnova <v.chesnova@icloud.com>
2022-09-12 22:20:46 +0200

    Corrected typo

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function formatDate(timestamp) {
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
