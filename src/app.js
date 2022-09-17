da368b03051ad789ad57121d20a581e64a0c9b4e
vi-chesnova <v.chesnova@icloud.com>
2022-09-17 14:49:56 +0200

    Changed forecast font size

--- src/app.js
+++ src/app.js
@@ -37,16 +37,16 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col">
+      <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
           src="src/img/${forecastDay.weather[0].icon}.png"
           alt=""
-          width="60"
+          width="50"
         />
      <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
